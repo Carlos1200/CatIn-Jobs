0377eeada893ff4e0a2001cd7b61fc2e3f10da01
Gerardo Velasquez <gerardo.jose2011@yahoo.es>
2022-02-27 23:57:59 -0600

    Haciendo el primer Push
    :D

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -24,11 +24,11 @@ class CreateUsersTable extends Migration
             $table->rememberToken();
             $table->foreignId('current_team_id')->nullable();
             $table->string('profile_photo_path', 2048)->nullable();
-            $table->string('rol',100);
+            $table->string('rol', 100);
             $table->timestamps();
         });
     }
-
+    //Send Help
     /**
      * Reverse the migrations.
      *
