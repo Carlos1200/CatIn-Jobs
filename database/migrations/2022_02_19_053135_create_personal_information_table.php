17620ad9b5e7c4415669ba9fe7c6b7a302eb4ea3
Mariella <mariiellachiicas14@gmail.com>
2022-03-18 22:52:02 -0600

    Punto uno modificado

--- database/migrations/2022_02_19_053135_create_personal_information_table.php
+++ database/migrations/2022_02_19_053135_create_personal_information_table.php
@@ -20,11 +20,7 @@ class CreatePersonalInformationTable extends Migration
             $table->date('birthday');
             $table->string('nationality',75);
             $table->string('phone_contact',15);
-<<<<<<< HEAD
-            $table->text('about_me');
-=======
             $table->text('about_me')->nullable();
->>>>>>> main
             $table->timestamps();
         });
     }
