fd26f0e681a8c7c204ee13440af6c4156027f041
Gerardo Velasquez <gerardo.jose2011@yahoo.es>
2022-03-19 00:32:19 -0600

    Empezando el Dashboard con medio Tailwind
    Literalmente no me importa todas las funciones de Tailwind por ende he trabajado con los unicos componentes que me importo :c

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,9 +1,41 @@
 <x-app-layout>
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                <x-jet-welcome />
+    <div class="mt-5 ">
+        <div class="flex flex-row m">
+            <div class="text-white">
+                <!-- Caja de Mis Empleados Aplicados -->
+                <div class="bg-gray-600 border-gray-200 border-2 rounded-lg m-7 p-6">
+                    <!-- Titulo -->
+                    <div class="flex flex-row items-center text-xl mb-4">
+                        <i class="fa-solid fa-bookmark mr-3"></i>
+                        <label>Mis Empleos Guardados</label>
+                    </div>
+                    <!-- Empleos Guardaos -->
+                    <div class="flex flex-col text-justify text-md">
+                        <label>- Desarrollador Web con PHP</label>
+                        <label>- Desarrollador Web con PHP</label>
+                        <label>- Desarrollador Web con PHP</label>
+                        <label>- Desarrollador Web con PHP</label>
+                        <label>- Desarrollador Web con PHP</label>
+                        <label>- Desarrollador Web con PHP</label>
+                        <label>- Desarrollador Web con PHP</label>
+                    </div>
+                    <!--Mostrar Mas -->
+                    <div class="flex flex-col justify-center items-center cursor-pointer mt-4">
+                        <p class="text-xl text-white">Mostrar Más</p>
+                        <i class="fa-solid fa-sort-down text-3xl text-white"></i>
+                    </div>
+                </div>
+                <!-- Boton Publicar Nuevo Empleo -->
+                <div class="flex flex-row items-center border-gray-200 border-2 rounded-lg m-7 p-6">
+                    <i class="fa-solid fa-pen-to-square mr-3 text-xl"></i>
+                    <p class="text-xl font-bold ">Publicar un Nuevo Empleo</p>
+                </div>
+            </div>
+
+            <!-- Oportunidades de Empleo a tu Alcance -->
+            <div class="m-5">
+                <div>Oportunidades de Empleo</div>
             </div>
         </div>
-    </div>
+
 </x-app-layout>
