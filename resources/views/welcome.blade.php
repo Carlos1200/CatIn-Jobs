47d58a3d9e85976ed2a42f6dc8956403accecd66
Mariella <mariiellachiicas14@gmail.com>
2022-03-26 16:20:36 -0600

    pagina principal

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -72,8 +72,16 @@
                             <div class="ml-12">
                                 <div class="mt-2 text-gray-600 dark:text-gray-400 text-sm">
                                     En Cat In Jobs existen dos modulos de usuarios:
-                                    -Modulo de empleador: este modulo consiste
-
+                                    <br>
+                                    <br>
+                                    -Modulo de empleador: este modulo consiste en que el usuario se registre como una 
+                                    empresa en busca de curriculums de personas aptas para las plazas que estan vacantes o solo publicar
+                                    las plazas para que el empleado aplique a estas plazas.
+                                    <br>
+                                    <br>
+                                    -Modulo de empleado: este modulo consiste en que el usuario cree una cuenta personal donde 
+                                    pueda subir su curriculum y buscar plazas vacantes de diferentes 
+                                    empresas.
                                      </div>
                             </div>
                         </div>
@@ -86,20 +94,32 @@
 
                             <div class="ml-12">
                                 <div class="mt-2 text-gray-600 dark:text-gray-400 text-sm">
-                                    Laravel News is a community driven portal and newsletter aggregating all of the latest and most important news in the Laravel ecosystem, including new package releases and tutorials.
-                                </div>
+                                <img src="{{ asset('images/noti1.jpg') }}" alt="Logo" class="w-52" width="375" height="300">
+                            
+                            </div>
                             </div>
                         </div>
 
                         <div class="p-6 border-t border-gray-200 dark:border-gray-700 md:border-l">
                             <div class="flex items-center">
-                                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" class="w-8 h-8 text-gray-500"><path d="M3.055 11H5a2 2 0 012 2v1a2 2 0 002 2 2 2 0 012 2v2.945M8 3.935V5.5A2.5 2.5 0 0010.5 8h.5a2 2 0 012 2 2 2 0 104 0 2 2 0 012-2h1.064M15 20.488V18a2 2 0 012-2h3.064M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
-                                <div class="ml-4 text-lg leading-7 font-semibold text-gray-900 dark:text-white">Vibrant Ecosystem</div>
+                            <i class="fa-solid fa-earth-americas"></i>
+                                <div class="ml-4 text-lg leading-7 font-semibold text-gray-900 dark:text-white">LINK INTERESANTES</div>
                             </div>
 
                             <div class="ml-12">
                                 <div class="mt-2 text-gray-600 dark:text-gray-400 text-sm">
-                                    Laravel's robust library of first-party tools and libraries, such as <a href="https://forge.laravel.com" class="underline">Forge</a>, <a href="https://vapor.laravel.com" class="underline">Vapor</a>, <a href="https://nova.laravel.com" class="underline">Nova</a>, and <a href="https://envoyer.io" class="underline">Envoyer</a> help you take your projects to the next level. Pair them with powerful open source libraries like <a href="https://laravel.com/docs/billing" class="underline">Cashier</a>, <a href="https://laravel.com/docs/dusk" class="underline">Dusk</a>, <a href="https://laravel.com/docs/broadcasting" class="underline">Echo</a>, <a href="https://laravel.com/docs/horizon" class="underline">Horizon</a>, <a href="https://laravel.com/docs/sanctum" class="underline">Sanctum</a>, <a href="https://laravel.com/docs/telescope" class="underline">Telescope</a>, and more.
+                                  Otras aplicaciones sobre empleo:
+                                  <br>  
+                                  <a href="https://www.tecoloco.com.sv" class="underline">- TECOLOCO</a>
+                                  <br> 
+                                  <a href="https://www.sv.computrabajo.com" class="underline">- COMPUTRABAJO</a>
+                                  <br> 
+                                  <a href="https://www.linkedin.com" class="underline">- LINKEDIN</a>
+                                  <br> 
+                                  <br> 
+                                  <img src="{{ asset('images/teco.jpg') }}" alt="Logo" class="w-52" width="75" height="75">
+                                  <img src="{{ asset('images/compu.jpg') }}" alt="Logo" class="w-52" width="75" height="75">
+                                  <img src="{{ asset('images/LINK.jpg') }}" alt="Logo" class="w-52" width="75" height="75">
                                 </div>
                             </div>
                         </div>
